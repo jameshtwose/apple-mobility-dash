42ada58ec4d5a8a4da4b2f7d6278aef03e7093d0
James Twose <jamestwose@Mac-mini.home>
2021-12-11 13:30:00 +0100

    creating the EDA file - looking into fluctuation intensity

--- EDA_apple_mobility.py
+++ EDA_apple_mobility.py
@@ -1,9 +1,13 @@
+#%%
 import json
 from urllib.request import urlopen
 import pandas as pd
 import numpy as np
 from jmspack.NLTSA import fluctuation_intensity
+from jmspack.utils import apply_scaling
+import seaborn as sns
 
+#%%
 def request_mobility_data_url():
     url = "https://covid19-static.cdn-apple.com/covid19-mobility-data/current/v3/index.json"
     response = urlopen(url)
@@ -13,6 +17,7 @@ def request_mobility_data_url():
 
 df = pd.read_csv(request_mobility_data_url())
 
+#%%
 prep_df = (df.drop(['geo_type',
  'alternative_name',
  'sub-region'], axis=1)
@@ -23,3 +28,7 @@ prep_df = (df.drop(['geo_type',
 
 prep_df.columns = ['-'.join(col).strip() for col in prep_df.columns.tolist()]
 
+#%%
+# fluctuation_intensity(df=prep_df, win=7)
+prep_df.filter(regex="Netherlands").astype(float).pipe(apply_scaling)
+# %%
